705007ede1297dd5155fedfabf5a2cbdb6497056
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-07-22 22:43:22 -0700

    gotta parse the game declaration WIP

--- api/api.go
+++ api/api.go
@@ -1,15 +1,32 @@
 package api
 
 import (
+	"fmt"
+	"mez/json"
 	"net/http"
 )
+
+type Rule struct{}
+
+type Koan struct{}
 
 func Instructions(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("How to play:\n		POST /game {rule: [<TODO>]} to start game"))
 }
 
 func CreateGame(w http.ResponseWriter, r *http.Request) {
+	parsed, err := json.Parse(r.Body)
+	if err != nil {
+		http.Error(w, "malformed JSON", 400)
+	} else {
+		fmt.Println(parsed)
+		rule, correct, incorrect := parseRule(parsed)
+		fmt.Println(rule, correct, incorrect)
+	}
+}
 
+func parseRule(data map[string]interface{}) (Rule, Koan, Koan) {
+	return Rule{}, Koan{}, Koan{}
 }
 
 func ViewGame(w http.ResponseWriter, r *http.Request)   {}
---
+++ json/parse.go
@@ -0,0 +1,16 @@
+package json
+
+import (
+	"encoding/json"
+	"io"
+)
+
+func Parse(data io.Reader) (map[string]interface{}, error) {
+	var jsonHolder interface{}
+	var parsedMap map[string]interface{}
+	err := json.NewDecoder(data).Decode(&jsonHolder)
+	if err == nil {
+		parsedMap = jsonHolder.(map[string]interface{})
+	}
+	return parsedMap, err
+}
