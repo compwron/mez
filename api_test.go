71890e700c52d1788ef361c60c561eea0a1742f0
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-07-30 22:42:24 -0700

    test rule count

--- api_test.go
+++ api_test.go
@@ -15,13 +15,17 @@ func verify(rule string, koan string, t *testing.T) {
 func TestSimplestKoanWithSimplestRule(t *testing.T) {
 	rule := "1^" 
 	koan := "1^SG"
-
 	verify(rule, koan, t)
 }
 
 func TestTwoPiecesShouldMatchAtLeastOnePieceRule(t *testing.T) {
 	rule := "1^" 
 	koan := "2^SG"
+	verify(rule, koan, t)
+}
 
+func TestOnePieceShouldNotMatchTwoPieceRule(t *testing.T) {
+ 	rule := "2^" 
+	koan := "1^SG"
 	verify(rule, koan, t)
 }
