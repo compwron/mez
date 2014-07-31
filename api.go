1077bf47be3b4a9e958b81a9aea80e3f79d79c02
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-07-30 22:41:21 -0700

    messily passing test for koan with two pieces pasing a koan requiring at least one piece

--- api.go
+++ api.go
@@ -80,7 +80,26 @@ func CreateKoan(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func intOf(char string) (int64, error) {
+	return strconv.ParseInt(char, 10, 8)
+}
+
 func doesKoanFulfillRule(rule Rule, koan string) bool {
+	ruleCharacters := strings.Split(rule.ruleDescriptions[0], "")
+	koanCharacters := strings.Split(koan, "")
+	
+	rulePieceCount, ruleErr := intOf(ruleCharacters[0])
+	koanPieceCount, koanErr := intOf(koanCharacters[0])
+	
+
+	if ruleErr != nil || koanErr != nil {
+		return false
+	}
+
+	if koanPieceCount > rulePieceCount {
+		return true
+	}
+
 	if strings.Contains(koan, currentRule.ruleDescriptions[0]) {
 		return true
 	}
