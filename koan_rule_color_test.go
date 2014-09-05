ad1a341d70a2c6ff1c8ae0ef960d86358f1caead
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-09-05 14:47:34 -0700

    Failing tests for excluding a certain number of certain colored pieces

--- koan_rule_color_test.go
+++ koan_rule_color_test.go
@@ -5,7 +5,24 @@ import (
 )
 
 // * "!G" No pieces may be green
-// * "!2G" One or three etc pieces may be green, but not two pieces.
+
+func TestExcludeCertainNumberOfCertainColorPiecesFail(t *testing.T) {
+	rule := "!2G"
+	koan := "1^SG,1^SG"
+	falsify(rule, koan, t)
+}
+
+func TestExcludeCertainNumberOfCertainColorPiecesPassWithLargerNumberOfPieces(t *testing.T) {
+	rule := "!2G"
+	koan := "1^SG,1^SG,1^SG"
+	verify(rule, koan, t)
+}
+
+func TestExcludeCertainNumberOfCertainColorPiecesPassWithSmallerNumberOfPieces(t *testing.T) {
+	rule := "!3G"
+	koan := "1^SG,1^SG"
+	verify(rule, koan, t)
+}
 
 func TestOnePieceMustBeGreenPass(t *testing.T) {
 	rule := "G" // At least one piece must be green
