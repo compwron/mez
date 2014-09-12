deb3a798ae4dda334f8a858873b120f5ae547a58
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-09-12 13:36:48 -0700

    Adding example explanations to rulegen script

--- rulegen.rb
+++ rulegen.rb
@@ -14,3 +14,8 @@ p (0..rand(rule_piece_count)).map {
 		values[rand(values.size)]
 	}.join("")
 }.join(",")
+
+puts "Example rule explanation: \n"
+	+ "\n 1^!3B means there must be one or more pieces, but there may not be exactly three blue pieces."
+	+ "\n !4^!4G means that there must not be exactly four pieces (but ther can be more or less), and there may not be exactly four green pieces (rule is kind of redundant)"
+	+ "\n 3^!4R,!1^3G means that there must be 3 or more pieces but not 4 red pieces. Also, there may not be exactly one piece and there must be at least 3 green pieces."
