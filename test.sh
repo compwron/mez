accf369ee385ffdc9f2b8bd1a0753cbce4c08164
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-09-12 18:23:57 -0700

    bash test script to hit the running server

---
+++ test.sh
@@ -0,0 +1,9 @@
+curl -X GET http://localhost:3000/game 
+curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"1^SG\"}" 
+curl -X POST http://localhost:3000/game/guess --data "{\"rule\":\"1^\"}" 
+curl -X POST http://localhost:3000/game --data "{\"rule\": \"2^\", \"true\":\"3^G\", \"false\":\"1^G\"}" 
+curl -X POST http://localhost:3000/game --data "{\"rule\": \"2^\", \"true\":\"3^G\", \"false\":\"1^G\"}" 
+curl -X POST http://localhost:3000/game/guess --data "{\"rule\":\"2^\"}" 
+curl -X POST http://localhost:3000/game/generate 
+curl -X POST http://localhost:3000/game/generate 
+curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"1^SG\"}" 
