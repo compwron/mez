74b3a53c8745a7691074856ba0d6546cfc625c2f
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-09-17 12:28:04 -0700

    more stuff in the test script

--- test.sh
+++ test.sh
@@ -3,7 +3,11 @@ curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"1^SG\"}"
 curl -X POST http://localhost:3000/game/guess --data "{\"rule\":\"1^\"}" 
 curl -X POST http://localhost:3000/game --data "{\"rule\": \"2^\", \"true\":\"3^G\", \"false\":\"1^G\"}" 
 curl -X POST http://localhost:3000/game --data "{\"rule\": \"2^\", \"true\":\"3^G\", \"false\":\"1^G\"}" 
+curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"1^SG\"}" 
 curl -X POST http://localhost:3000/game/guess --data "{\"rule\":\"2^\"}" 
+curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"1^SG\"}" 
 curl -X POST http://localhost:3000/game/generate 
+curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"1^SG\"}" 
+curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"3^SG\"}" 
 curl -X POST http://localhost:3000/game/generate 
 curl -X POST http://localhost:3000/game/koan --data "{\"koan\":\"1^SG\"}" 
